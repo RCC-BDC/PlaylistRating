28f39159ae995ff199fb0ef4e18418c2b7b13136
rcc_bdc <bdcepas@yahoo.com>
2024-10-05 13:27:14 -0400

    Added search bar

--- PlaylistCollab/frontend/src/components/HomePage.js
+++ PlaylistCollab/frontend/src/components/HomePage.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const HomePage = () => {
+
+    const[playlistId, setPlaylistId] = useState('');
+
+    const handleIdChange = (text) => {
+        console.log(text);
+        setPlaylistId(text.target.value);
+    }
+    
+    const handleSearchClick = () => {
+        console.log('Click');
+    }
+
     return(
-        <div>
-            <h1>We out here</h1>
+        <div className="page bg-light">
+            <div className="container">
+                <div className="d-flex flex-row justify-content-center">
+                    <h1>Find a Playlist</h1>
+                </div>
+                <div className="d-flex flex-row justify-content-center">
+                    <label for="Find Playlist"></label> <br />
+                    <input autofocus id="partyCode" type="text" placeholder="Playlist ID" onChange={handleIdChange}/> <br />
+                    <button className="btn btn-primary" type="button" onClick={handleSearchClick}>Search</button>
+                </div>
+            </div>
         </div>
     )
 }
